1f0e0b2bc52ec412fa2d89a893d36fe4edae2932
firstzerg <chronoas@mail.ru>
2022-11-20 10:47:21 +0300

    ver update

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
 [assembly: AssemblyProduct("SiteWatcher")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyCopyright("Copyright © 2022")]
-[assembly: AssemblyVersion("0.9.1.2")]
-[assembly: AssemblyFileVersion("0.9.1.2")]
+[assembly: AssemblyVersion("0.9.1.3")]
+[assembly: AssemblyFileVersion("0.9.1.3")]
