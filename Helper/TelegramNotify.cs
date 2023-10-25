fc178f447533bdeceeb680f3818cbe470420a500
hronoas <chronoas@mail.ru>
2023-10-25 20:42:15 +0300

    добавлено: условия в шаблонах телеграма

--- Helper/TelegramNotify.cs
+++ Helper/TelegramNotify.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using CefSharp.DevTools.DOM;
+using System.Windows.Forms.VisualStyles;
 
 namespace SiteWatcher{
     public class TelegramConfig{
@@ -19,21 +23,18 @@ namespace SiteWatcher{
         public static async Task SendMessageAsync(TelegramConfig config, string text_template="", Dictionary<string,string>? data = null, string? chatId = null){
             chatId = string.IsNullOrWhiteSpace(chatId)?config.ChatId:chatId;
             if (string.IsNullOrWhiteSpace(chatId)) { 
-                Log("No chat id","telegram");
+                Log("No destination: empty chat_id","telegram");
                 return;
             }
             
             string messageText = string.IsNullOrWhiteSpace(text_template)?config.Template:text_template;
+            messageText = Replacer.replacePatterns(messageText,data);
+            if (messageText.Length>4000) messageText=messageText.Substring(0,4000);
+
             if (string.IsNullOrWhiteSpace(messageText)){
-                Log("Nothing to send","telegram");
+                Log("Nothing to send: empty message","telegram");
                 return;
             };
-
-            if (data!=null){
-                foreach (KeyValuePair<string,string> kv in data){
-                    messageText = messageText.Replace($"{{{kv.Key}}}", kv.Value.ToString());
-                }
-            }
             
             string uri = $"https://api.telegram.org/bot{config.BotToken}/sendMessage?chat_id={chatId}&text={Uri.EscapeDataString(messageText)}";
             HttpClient httpClient = new();
@@ -49,5 +50,67 @@ namespace SiteWatcher{
             }
             
         }
+    }
+
+
+    public static class Replacer{
+
+        private static Regex tag_regex = new Regex(@"{(?<tag>[a-z]+)(?<attribs>!?[=~][^}]*)}(?<content>.*?){\/\1}",RegexOptions.Singleline|RegexOptions.IgnoreCase);
+        private static Regex attribs_regex = new Regex(@"(?<mod>!)?(?=[=~])(?<comparator>[=~])(?:""(?<val1>.*?)(?<!\\)""|(?<val2>.*?)(?<!\\)(?=(!?)[=~]|$))",RegexOptions.Singleline|RegexOptions.IgnoreCase);
+        private static Regex stag_regex = new Regex(@"{(?<tag>[a-z]+)}",RegexOptions.Singleline|RegexOptions.IgnoreCase);
+
+        public static string replacePatterns(string pattern, Dictionary<string,string>? data=null){
+            
+            Match tag_match = tag_regex.Match(pattern);
+            while(tag_match.Success){
+                string tag_name = tag_match.Groups["tag"].Value.ToString().ToLower();
+                string tag_attribs= tag_match.Groups["attribs"].Value.ToString();
+                string tag_content = tag_match.Groups["content"].Value.ToString();
+                string replace_text = "";
+                if(data!=null && data.ContainsKey(tag_name))
+                    replace_text = replaceCompares(tag_name,tag_attribs,tag_content,data);
+                pattern = pattern.Replace(tag_match.Groups[0].Value,replace_text);
+                tag_match = tag_regex.Match(pattern);
+            }
+            return replaceTags(pattern,data);
+        }
+
+        private static string replaceCompares(string tag, string attribs, string value, Dictionary<string,string> data){
+            
+            MatchCollection matches = attribs_regex.Matches(attribs);
+            bool result = false;
+            if(matches.Count!=0){
+                foreach (Match attrib_match in matches){
+                    string val = (!string.IsNullOrEmpty(attrib_match.Groups["val1"].Value.ToString())?attrib_match.Groups["val1"].Value.ToString():attrib_match.Groups["val2"].Value.ToString()).ToLower();
+                    string check = data[tag].ToLower();
+                    bool inverse = attrib_match.Groups["mod"].Value.ToString()=="!";
+                    switch (attrib_match.Groups["comparator"].Value.ToString()){
+                        case "=":
+                            result |= inverse ? val!=check:val==check ;
+                            break;
+                        case "~":
+                            result |= inverse ? !check.Contains(val): check.Contains(val);
+                            break;
+                        default: 
+                            break;
+                    }
+                }
+            }else if (data!=null && data.ContainsKey(tag) && !string.IsNullOrWhiteSpace(data[tag])) result = true;
+            if(result)
+                return replacePatterns(value,data);
+            else 
+                return "";
+        }
+
+        private static string replaceTags(string text, Dictionary<string,string>? data){
+            
+            foreach (Match tag_match in stag_regex.Matches(text)){
+                string tag_name = tag_match.Groups["tag"].Value.ToString().ToLower();
+                if (data!=null && data.ContainsKey(tag_name))
+                    text = text.Replace('{'+tag_name+'}',data[tag_name]);
+            }
+            return text;
+        }
+
     }
 }
--- Window/AppWindowModel.Telegram.cs
+++ Window/AppWindowModel.Telegram.cs
@@ -7,14 +7,14 @@ using System.Media;
 namespace SiteWatcher
 {
     public partial class AppWindowModel : BaseWindowModel<AppWindow>{
-        private const string defaultTelegramTemplate = "{name} ({status})\n{url}\n{content}{error}";
+        private const string defaultTelegramTemplate = "{name} ({status})\n{url}\n{error=}{changed}{/error}{error}";
         private TelegramConfig telegram = new (){Template=defaultTelegramTemplate};
 
         private static Dictionary<string,Func<Watch,string>> defaultDataKeys = new(){
             {"status",(w)=>(new WatchStatusToStringConverter()).Convert(w.Status,typeof(string),"",CultureInfo.CurrentCulture) as string??""},
             {"name",(w)=>w.Name},
             {"url",(w)=>w.Source.Url},
-            {"content",(w)=>string.IsNullOrWhiteSpace(w.Error)?w.Diff.Next.Text:""},
+            {"content",(w)=>w.Diff.Next.Text},
             {"comment",(w)=>w.Comment},
             {"error",(w)=>w.Error},
             {"tags",(w)=>string.Join(", ",w.Tags.Select(t=>t.Name))},
@@ -26,10 +26,11 @@ namespace SiteWatcher
                 List<DiffPart> compare = DiffComparer.CompareStrings(w.Diff.Prev.Text,w.Diff.Next.Text,oneLevel:true);
                 return string.Join("\n",compare.Where(d=>d.Type==DiffType.Delete).Select(d=>d.Text));
             }}
-            
         };
         
-        public static string AvailableTelegramReplace {get;set;} = string.Join(", ",defaultDataKeys.Select(d=>"{"+d.Key+"}"));
+        public static string AvailableTelegramReplace {get;set;} = "Доступные замены: "+string.Join(", ",defaultDataKeys.Select(d=>"{"+d.Key+"}"))+"\n"+
+                                                                    "Проверкой значений: {status=Новое}ВНИМАНИЕ{/status} - выведет 'ВНИМАНИЕ' при статусе 'Новое'\n"+
+                                                                    "Операторы сравнения:\n'=' - совпадает\n'!=' - не совпадает\n'~' - содержит\n'!~' - не содержит";
 
         public void SendTelegram(Watch watch){
 
