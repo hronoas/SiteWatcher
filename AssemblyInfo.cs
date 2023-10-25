276cec7482ab64f7a76b50ffee903ad2063bcd69
hronoas <chronoas@mail.ru>
2023-10-25 20:43:48 +0300

    номер версии

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Windows;
 [assembly: AssemblyProduct("SiteWatcher")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyCopyright("Copyright © 2023")]
-[assembly: AssemblyVersion("0.9.2.2")]
-[assembly: AssemblyFileVersion("0.9.2.2")]
+[assembly: AssemblyVersion("0.9.2.3")]
+[assembly: AssemblyFileVersion("0.9.2.3")]
